5d9008fd1222235e7a7923067f7f0d234aaf1a46
ania.brown <ania.brown@gmail.com>
2017-05-09 14:50:13 +0100

    added precision file

---
+++ QUEST/precision.h
@@ -0,0 +1,28 @@
+# ifndef PRECISION
+# define PRECISION
+
+// *** EDIT PRECISION HERE
+// OPTIONS: 1, 2, 4
+# define P 2
+
+# if P==1
+	// SINGLE PRECISION
+	# define REAL float
+	# define MPI_REAL MPI_FLOAT
+	# define REAL_STRING_FORMAT "%.8f"
+# elif P==4
+	// QUAD PRECISION
+	// 80-bit precision for most implementations
+	# define REAL long double
+	# define MPI_REAL MPI_LONG_DOUBLE
+	# define REAL_STRING_FORMAT "%.20Lf"
+# else
+	// DOUBLE PRECISION
+	# define REAL double
+	# define MPI_REAL MPI_DOUBLE
+	//# define REAL_STRING_FORMAT "%.14f"
+	# define REAL_STRING_FORMAT "%.20f"
+# endif
+
+
+# endif
