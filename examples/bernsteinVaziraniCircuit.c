28d59acb692819da5675c677e074ea09e7407410
Tyson Jones <tyson.jones@monash.edu>
2018-01-29 17:32:21 +0000

    Create bernsteinVaziraniCircuit.c
    Added an example circuit implementing the Bernstein-Vazirani algorithm

---
+++ examples/bernsteinVaziraniCircuit.c
@@ -0,0 +1,76 @@
+/** @file 
+ * Implements the Bernstien--Vazirani circuit
+ */
+
+# include <stdio.h>
+# include <math.h>
+# include <omp.h>
+
+# include "../QuEST/qubits.h"   // TODO: repoint to your direc
+
+int main (int narg, char** varg) {
+
+	
+	/* 	
+	 * PREPARE QuEST
+	 */
+	
+	// model parameters
+	int numQubits = 9;
+	int secretNum = pow(2,4) + 1;
+	
+	// prepare QuEST
+	QuESTEnv env;
+	initQuESTEnv(&env);
+	
+	// create register; let zeroth qubit be ancilla
+	MultiQubit multiQubit; 
+	createMultiQubit(&multiQubit, numQubits, env);
+	initStateZero (&multiQubit);
+
+
+	/* 	
+	 * APPLY ALGORITHM
+	 */
+	 
+	// NOT the ancilla
+	sigmaX(multiQubit, 0);
+	
+	// CNOT secretNum bits with ancilla
+	int bits = secretNum;
+	int bit;
+	for (int qb=1; qb < numQubits; qb++) {
+		bit = bits % 2;
+		bits /= 2;
+		if (bit)
+			controlNot(multiQubit, qb, 0);
+	}
+	
+	
+	/* 	
+	 * VERIFY FINAL STATE
+	 */
+	 
+	 // calculate prob of solution state
+	double successProb = 1.0;
+	bits = secretNum;
+	for (int qb=1; qb < numQubits; qb++) {
+		bit = bits % 2;
+		bits /= 2;
+		successProb *= findProbabilityOfOutcome(
+			multiQubit, qb, bit);
+	}
+
+	printf("solution reached with probability ");
+	printf("%f", successProb);
+	printf("\n");
+	
+
+	/*
+	 * FREE MEMORY
+	 */
+	
+	destroyMultiQubit(multiQubit, env); 
+	closeQuESTEnv(env);
+	return 0;
+}
