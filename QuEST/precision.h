dcf2aebff048a908689477a657fd246ea5f2168a
Tyson Jones <tyson.jones.input@gmail.com>
2018-04-02 14:55:21 +0100

    Renamed P to PREC to avoid GSL clash

--- QuEST/precision.h
+++ QuEST/precision.h
@@ -5,15 +5,15 @@
 
 // *** EDIT PRECISION HERE
 // OPTIONS: 1, 2, 4
-# define P 2
+# define PREC 2
 
-# if P==1
+# if PREC==1
 	// SINGLE PRECISION
 	# define REAL float
 	# define MPI_QuEST_REAL MPI_FLOAT
 	# define REAL_STRING_FORMAT "%.8f"
 	# define REAL_EPS 1e-5
-# elif P==4
+# elif PREC==4
 	// QUAD PRECISION
 	// 80-bit precision for most implementations
 	# define REAL long double
