5222287dea07609b7c533c96efdd9b1587a8ac2d
TysonRayJones <tyson.jones.input@gmail.com>
2018-07-11 01:13:48 +0100

    added compilation tester
    Tests the compilation and running of QuEST with different versions of C/C++ compilers and CUDA/NVCC wrappers

---
+++ tests/compilers/testcode.cpp
@@ -0,0 +1,19 @@
+#include <QuEST.h>
+
+int main (int narg, char *varg[]) {
+
+    QuESTEnv env;
+	MultiQubit qubits; 
+    initQuESTEnv(&env);
+    createMultiQubit(&qubits, 3, env);
+    initStateZero(&qubits);
+
+    hadamard(qubits, 0);
+    controlledNot(qubits, 0, 1);
+    rotateY(qubits, 2, .1);
+
+    destroyMultiQubit(qubits, env); 
+    closeQuESTEnv(env);
+
+    return 0;
+}
