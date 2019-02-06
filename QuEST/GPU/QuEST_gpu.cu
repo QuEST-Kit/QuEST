QuEST-Kit/QuEST

commit b6078732163dab7a2d8ee6d51108da0d79aad22b

fixed bug in seedQuESTDefault in MPI mode.
Previously different processes could receive different random number seeds, thus potentially disagreeing on measurement outcomes in measure, measureWithStats.

Fixed by moving seedQuESTDefault to architecture specific files (QuEST_cpu_distributed.c, QuEST_cpu_local.c and QuEST_gpu.cu). In order to not have to set up include files for creating the seeds in each place, the bulk of the function is kept in QuEST_common.c as getQuESTDefaultSeedKey.

QuEST/GPU/QuEST_gpu.cu
@@ -2218,6 +2218,16 @@ void densmatr_twoQubitDepolarise(Qureg qureg, int qubit1, int qubit2, qreal depo
         part1, part2, part3, part4, part5, rowCol1, rowCol2);
 }
 
+void seedQuESTDefault(){
+    // init MT random number generator with three keys -- time, pid and a hash of hostname 
+    // for the MPI version, it is ok that all procs will get the same seed as random numbers will only be 
+    // used by the master process
+
+    unsigned long int key[3];
+    getQuESTDefaultSeedKey(key); 
+    init_by_array(key, 3); 
+}  
+
 
 #ifdef __cplusplus
 }

QuEST/CPU/QuEST_cpu_distributed.c
@@ -1269,3 +1269,16 @@ void statevec_collapseToKnownProbOutcome(Qureg qureg, const int measureQubit, in
         }
     }
 }
+
+void seedQuESTDefault(){
+    // init MT random number generator with three keys -- time, pid and a hash of hostname 
+    // for the MPI version, it is ok that all procs will get the same seed as random numbers will only be 
+    // used by the master process
+
+    unsigned long int key[3];
+    getQuESTDefaultSeedKey(key);
+    // this seed will be used to generate the same random number on all procs,
+    // therefore we want to make sure all procs receive the same key
+    MPI_Bcast(key, 3, MPI_LONG_INT, 0, MPI_COMM_WORLD);
+    init_by_array(key, 3);
+}

QuEST/CPU/QuEST_cpu_local.c
@@ -281,3 +281,13 @@ void statevec_collapseToKnownProbOutcome(Qureg qureg, const int measureQubit, in
 {
     statevec_collapseToKnownProbOutcomeLocal(qureg, measureQubit, outcome, stateProb);
 }
+
+void seedQuESTDefault(){
+    // init MT random number generator with three keys -- time, pid and a hash of hostname 
+    // for the MPI version, it is ok that all procs will get the same seed as random numbers will only be 
+    // used by the master process
+
+    unsigned long int key[3];
+    getQuESTDefaultSeedKey(key);
+    init_by_array(key, 3);
+}

QuEST/QuEST_common.c
@@ -132,7 +132,7 @@ unsigned long int hashString(char *str){
     return hash;    
 }
 
-void seedQuESTDefault(){
+void getQuESTDefaultSeedKey(unsigned long int *key){
     // init MT random number generator with three keys -- time, pid and a hash of hostname 
     // for the MPI version, it is ok that all procs will get the same seed as random numbers will only be 
     // used by the master process
@@ -140,7 +140,7 @@ void seedQuESTDefault(){
     struct timeval  tv;
     gettimeofday(&tv, NULL);
 
-    double time_in_mill = 
+    double time_in_mill =
         (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000 ; // convert tv_sec & tv_usec to millisecond
 
     unsigned long int pid = getpid();
@@ -149,9 +149,7 @@ void seedQuESTDefault(){
     gethostname(hostName, sizeof(hostName));
     unsigned long int hostNameInt = hashString(hostName);
 
-    unsigned long int key[3];
     key[0] = msecs; key[1] = pid; key[2] = hostNameInt;
-    init_by_array(key, 3); 
 }
 
 /** 

QuEST/QuEST_internal.h
@@ -37,6 +37,8 @@ void getComplexPairAndPhaseFromUnitary(ComplexMatrix2 u, Complex* alpha, Complex
 
 void shiftIndices(int* indices, int numIndices, int shift);
 
+void getQuESTDefaultSeedKey(unsigned long int *key);
+
 
 /*
  * operations upon density matrices 

examples/tutorial_example.c
@@ -1,91 +1,0 @@
-#include <stdio.h>
-#include "QuEST.h"
-
-int main (int narg, char *varg[]) {
-
-    /*
-     * PREPARE QuEST environment
-     * (Required only once per program)
-     */
-
-    QuESTEnv env = createQuESTEnv();
-
-    printf("-------------------------------------------------------\n");
-    printf("Running QuEST tutorial:\n\t Basic circuit involving a system of 3 qubits.\n");
-    printf("-------------------------------------------------------\n");
-
-    /*
-     * PREPARE QUBIT SYSTEM
-     */
-
-    Qureg qubits = createQureg(3, env);
-    initZeroState(qubits);
-
-
-    /*
-     * REPORT SYSTEM AND ENVIRONMENT
-     */
-    printf("\nThis is our environment:\n");
-    reportQuregParams(qubits);
-    reportQuESTEnv(env);
-
-    /*
-     * APPLY CIRCUIT
-     */
-
-    hadamard(qubits, 0);
-    controlledNot(qubits, 0, 1);
-    rotateY(qubits, 2, .1);
-
-    multiControlledPhaseFlip(qubits, (int []){0, 1, 2}, 3);
-
-    ComplexMatrix2 u;
-    u.r0c0 = (Complex) {.real=.5, .imag= .5};
-    u.r0c1 = (Complex) {.real=.5, .imag=-.5}; 
-    u.r1c0 = (Complex) {.real=.5, .imag=-.5};
-    u.r1c1 = (Complex) {.real=.5, .imag= .5};
-    unitary(qubits, 0, u);
-
-    Complex a, b;
-    a.real = .5; a.imag =  .5;
-    b.real = .5; b.imag = -.5;
-    compactUnitary(qubits, 1, a, b);
-
-    Vector v;
-    v.x = 1; v.y = 0; v.z = 0;
-    rotateAroundAxis(qubits, 2, 3.14/2, v);
-
-    controlledCompactUnitary(qubits, 0, 1, a, b);
-
-    multiControlledUnitary(qubits, (int []){0, 1}, 2, 2, u);
-
-    printf("\nCircuit output:\n");
-
-    qreal prob;
-    prob = getProbAmp(qubits, 7);
-    printf("Probability amplitude of |111>: %f\n", prob);
-
-    prob = calcProbOfOutcome(qubits, 2, 1);
-    printf("Probability of qubit 2 being in state 1: %f\n", prob);
-
-    int outcome = measure(qubits, 0);
-    printf("Qubit 0 was measured in state %d\n", outcome);
-
-    outcome = measureWithStats(qubits, 2, &prob);
-    printf("Qubit 2 collapsed to %d with probability %f\n", outcome, prob);
-
-
-    /*
-     * FREE MEMORY
-     */
-
-    destroyQureg(qubits, env); 
-
-
-    /*
-     * CLOSE QUEST ENVIRONMET
-     * (Required once at end of program)
-     */
-    destroyQuESTEnv(env);
-    return 0;
-}

commit 700ef9b9a875d74b9f8721cc4852fe7afc34fe7d

Patching GPU QuEST build
(bug introduced in commit b6078732163dab7a2d8ee6d51108da0d79aad22b, when seeding was moved out of hardware backends)

QuEST/GPU/QuEST_gpu.cu
@@ -1,11 +1,12 @@
 // Distributed under MIT licence. See https://github.com/aniabrown/QuEST_GPU/blob/master/LICENCE.txt for details
 
 /** @file
- * An implementation of the pure backend in ../QuEST_ops_pure.h for a GPU environment.
+ * An implementation of the backend in ../QuEST_internal.h for a GPU environment.
  */
 
 # include "../QuEST.h"
 # include "../QuEST_precision.h"
+# include "../QuEST_internal.h"    // purely to resolve getQuESTDefaultSeedKey
 # include "../mt19937ar.h"
 
 # include <stdlib.h>