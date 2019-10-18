6f0ffd4cdceebf3c1de109ac72c803ecf96dc640
TysonRayJones <tyson.jones.input@gmail.com>
2019-10-18 15:51:07 +0100

    added Catch2 for unit testing

---
+++ testing/catch_main.cpp
@@ -0,0 +1,9 @@
+/** @file
+ * This file is left empty so that catch doesn't need 
+ * slow (~16s) recompilation each time unit tests are edited
+ *
+ * @author Tyson Jones
+ */
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
