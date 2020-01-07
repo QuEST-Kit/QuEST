8df2348dcf3290bfdd391076e7974150933f6158
TysonRayJones <tyson.jones.input@gmail.com>
2020-01-07 06:44:26 +0000

    Improving doc
    for
    - calcProbOfOutcome
    - calcInnerProduct
    - measureWithStats

--- QuEST/include/QuEST.h
+++ QuEST/include/QuEST.h
@@ -1671,6 +1671,15 @@ void controlledPauliY(Qureg qureg, const int controlQubit, const int targetQubit
 /** Gives the probability of a specified qubit being measured in the given outcome (0 or 1).
  * This performs no actual measurement and does not change the state of the qubits.
  * 
+ * For state-vectors, this function works by summing the absolute-value-squared of every 
+ * amplitude in the state-vector for which \p measureQubit \p = \p 0. 
+ * If \p outcome \p = \p 1, it returns \p 1 minus this value. Hence for unnormalised 
+ * state-vectors, this result will differ from the absolute-value-squared of every 
+ * amplitude where \p measureQubit \p = \p outcome.
+ *
+ * For density matrices, this function sums the diagonal values (should be real)
+ * corresponding to \p measureQubit \p = \p 0 (returning 1 minus this if \p outcome \p = \p 1).
+ * 
  * @ingroup calc
  * @param[in] qureg object representing the set of all qubits
  * @param[in] measureQubit qubit to study
@@ -1684,12 +1693,18 @@ void controlledPauliY(Qureg qureg, const int controlQubit, const int targetQubit
  */
 qreal calcProbOfOutcome(Qureg qureg, const int measureQubit, int outcome);
 
-/** Updates the state vector to be consistent with measuring the measure qubit in the given outcome (0 or 1), and returns the probability of such a measurement outcome. 
- * This is effectively performing a measurement and forcing the outcome.
- * This is an irreversible change to the state vector, whereby incompatible states
- * in the state vector are given zero amplitude and the remaining states are renormalised.
- * Exits with error if the given outcome has ~zero probability, and so cannot be
+/** Updates \p qureg to be consistent with measuring \p measureQubit in the given 
+ * \p outcome (0 or 1), and returns the probability of such a measurement outcome. 
+ * This is effectively performing a projection, or a measurement with a forced outcome.
+ * This is an irreversible change to the state, whereby computational states
+ * inconsistant with the outcome are given zero amplitude and the \p qureg is renormalised.
+ * Exits with error if the given outcome has a near zero probability, and so cannot be
  * collapsed into.
+ *
+ * Note that the collapse probably used for renormalisation is calculated for 
+ * \p outcome \p = \p 0, and assumed 1 minus this probability if \p outcome \p = \p 1.
+ * Hence this routine will not correctly project un-normalised quregs onto 
+ * \p outcome \p = \p 1.
  * 
  * @ingroup normgate
  * @param[in,out] qureg object representing the set of all qubits
@@ -1738,10 +1753,17 @@ int measure(Qureg qureg, int measureQubit);
 int measureWithStats(Qureg qureg, int measureQubit, qreal *outcomeProb);
 
 /** Computes the inner product \f$ \langle \text{bra} | \text{ket} \rangle \f$ of two 
- * equal-size state vectors. The same \p qureg may be passed as both \p bra and \p ket, 
+ * equal-size state vectors, given by 
+ * \f[ 
+     \langle \text{bra} | \text{ket} \rangle = \sum_i {\text{bra}_i}^* \; \times \; \text{ket}_i 
+ * \f]
+ * The same \p qureg may be passed as both \p bra and \p ket, 
  * though we recommend users check state-vector normalisation with \p calcTotalProb which 
  * employs Kahan summation for greater accuracy.
  * Neither state-vector is modified.
+ *
+ * This function returns the correct inner product even if \p bra and \p ket are 
+ * not correctly normalised states.
  *
  * @ingroup calc
  * @param[in] bra qureg to be the 'bra' (i.e. have its values conjugate transposed) in the inner product 
@@ -2402,7 +2424,7 @@ void multiRotatePauli(Qureg qureg, int* targetQubits, enum pauliOpType* targetPa
  * applies to the least-significant qubit, i.e. that with index 0).
  *
  * \p workspace must be a register with the same type (statevector vs density matrix) and dimensions 
- * (numQubitsRepresented) as \p qureg, and is used as working space. When this function returns, \p qureg 
+ * (number of represented qubits) as \p qureg, and is used as working space. When this function returns, \p qureg 
  * will be unchanged and \p workspace will be set to \f$ \sigma | \psi \rangle \f$ (if \p qureg is a statevector)
  * or \f$ \sigma \rho \f$ (if \p qureg is a density matrix). NOTE that this last quantity is NOT the result of applying 
  * the paulis as unitaries, \f$ \sigma^\dagger \rho \sigma \f$, but is instead the result of their direct 
@@ -2454,7 +2476,7 @@ qreal calcExpecPauliProd(Qureg qureg, int* targetQubits, enum pauliOpType* pauli
  * applies to the least-significant qubit, i.e. that with index 0).
  * 
  * \p workspace must be a register with the same type (statevector vs density matrix) and dimensions 
- * (numQubitsRepresented) as \p qureg, and is used as working space. When this function returns, \p qureg 
+ * (number of represented qubits) as \p qureg, and is used as working space. When this function returns, \p qureg 
  * will be unchanged and \p workspace will be set to \p qureg pre-multiplied with the final Pauli product.
  * NOTE that if \p qureg is a density matrix, \p workspace will become \f$ \hat{\sigma} \rho \f$ 
  * which is itself not a density matrix (it is distinct from \f$ \hat{\sigma}^\dagger \rho \hat{\sigma} \f$).
@@ -3033,6 +3055,9 @@ void mixMultiQubitKrausMap(Qureg qureg, int* targets, int numTargets, ComplexMat
  * \f]
  * We caution this may differ by some definitions of the Hilbert Schmidt distance 
  * by a square-root.
+ *
+ * This function correctly returns the result of the above formulations even when 
+ * \p a and \p b are incorrectly normalised (i.e. are general matrices).
  *
  * @ingroup calc
  * @param[in] a a density matrix
