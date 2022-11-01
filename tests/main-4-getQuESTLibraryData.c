4061a2725df7b7e8b95de8f15a78f36abea44a14
dojt <dotheis.work@gmail.com>
2022-11-01 15:20:08 +0200

    tests/main-4-getQuESTLibraryData.c  -- NEW

---
+++ tests/main-4-getQuESTLibraryData.c
@@ -0,0 +1,39 @@
+#include <string.h>
+#include <stdio.h>
+
+int main(int, char*[]) {
+  quenv = createQuESTEnv();
+
+  {
+       QuESTLibraryData libdat;
+       printf("Testing `getQuESTLibraryData()`...\n"); fflush(stdout);
+       getQuESTLibraryData(quenv, &libdat);
+
+       printf("Test correctness of the precision info...\n"); fflush(stdout);
+       if ( libdat.QuEST_Prec == sizeof(qreal)/4 ) {
+            printf("... fine.\n");
+       } else {
+            printf("`getQuESTLibraryData()` gave %d but truth is %d.\n",
+                   libdat.QuEST_Prec, sizeof(qreal)/4);
+       }
+
+       printf("Test correctness of the rest via `getEnvironmentString()`...\n"); fflush(stdout);
+       char str  [256] = "";
+       char costr[256] = "";
+       getEnvironmentString(quenv, str);
+       sprintf(costr, "CUDA=%d OpenMP=%d MPI=%d threads=%d ranks=%d",
+               (int)libdat.CUDA, (int)libdat.OpenMP, (int)libdat.MPI,
+               (int)libdat.threads, (int)libdat.ranks);
+
+       if ( strncmp(str,costr,256) ){
+            str[255] = costr[255] = '\0';
+            printf("`getQuESTLibraryData()` gave\n\t%s\nbut `getEnvironmentString()` gave\n\t%s\n",
+                   costr, str);
+       } else {
+            printf("... fine.\n");
+       }
+  }
+
+  destroyQuESTEnv(quenv);
+  return 0;
+}
