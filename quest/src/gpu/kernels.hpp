4122a6ed4d6cd88305112c4e34624f3cb6549c3f
TysonRayJones <tyson.jones.input@gmail.com>
2024-06-09 10:59:13 +1000

    added scalar types
    - `qreal`
    - `qindex`
    - `qcomp`
    
    The latter is a C/C++ agnostic complex scalar compatible with OpenMP, MPI, Thrust, cuQuantum, and given a CUDA-compatible wrapper
    
    Co-Authored-By: Ali Rezaei <Ali.Rezaei@ed.ac.uk>

--- quest/src/gpu/kernels.hpp
+++ quest/src/gpu/kernels.hpp
@@ -7,6 +7,48 @@
 #ifndef KERNELS_HPP
 #define KERNELS_HPP
 
+#include "quest/include/modes.h"
+#include "quest/include/types.h"
+
+#include <cuComplex.h>
+
+
+
+/*
+ * CREATE CUDA-COMPATIBLE QCOMP ALIAS
+ */
+
+#if (FLOAT_PRECISION == 1)
+    typedef cuFloatComplex cu_qcomp;
+
+#elif (FLOAT_PRECISION == 2)
+    typedef cuDoubleComplex cu_qcomp;
+
+#else
+    #error "Build bug; precision.h should have prevented non-float non-double qcomp precision on GPU."
+
+#endif
+
+
+
+/*
+ * CREATE CUDA-COMPATIBLE QCOMP OVERLOADS
+ */
+
+__host__ __device__ inline cu_qcomp operator + (const cu_qcomp& a, const cu_qcomp& b) {
+    cu_qcomp res;
+    res.x = a.x + b.x;
+    res.y = a.y + b.y;
+    return res;
+}
+
+__host__ __device__ inline cu_qcomp operator * (const cu_qcomp& a, const cu_qcomp& b) {
+    cu_qcomp res;
+    res.x = a.x * b.x - a.y * b.y;
+    res.y = a.x * b.y + a.y * b.x;
+    return res;
+}
+
 
 
 #endif // KERNELS_HPP
