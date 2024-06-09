256c1906cfb942157a18fcf030f5e34feee2525d
TysonRayJones <tyson.jones.input@gmail.com>
2024-06-09 10:59:13 +1000

    added scalar types
    - `qreal`
    - `qindex`
    - `qcomp`
    
    The latter is a C/C++ agnostic complex scalar compatible with OpenMP, MPI, Thrust, cuQuantum, and given a CUDA-compatible wrapper
    
    Co-Authored-By: Ali Rezaei <Ali.Rezaei@ed.ac.uk>

--- quest/include/precision.h
+++ quest/include/precision.h
@@ -5,6 +5,90 @@
 #ifndef PRECISION_H
 #define PRECISION_H
 
+#include "quest/include/modes.h"
+
+
+
+/*
+ * STATE-INDEXING TYPE
+ */
+
+// can be (for example) int, long, long long, unsigned, long unsigned, long long unsigned
+#define INDEX_TYPE long long unsigned
+
+
+
+/*
+ * RE-CONFIGURABLE FLOATING-POINT PRECISION
+ */
+
+// assume double precision as default
+#ifndef FLOAT_PRECISION
+    #define FLOAT_PRECISION 2
+#endif
+
+// validate precision is 1 (float), 2 (double) or 4 (long double)
+#if ! (FLOAT_PRECISION == 1 || FLOAT_PRECISION == 2 || FLOAT_PRECISION == 4)
+    #error "FLOAT_PRECISION must be 1 (float), 2 (double) or 4 (long double)"
+#endif 
+
+// infer floating-point type from precision
+#if FLOAT_PRECISION == 1
+    #define FLOAT_TYPE float
+#elif FLOAT_PRECISION == 2
+    #define FLOAT_TYPE double
+#elif FLOAT_PRECISION == 4
+    #define FLOAT_TYPE long double
+#endif
+
+
+
+/*
+ * CHECK PRECISION TYPES ARE COMPATIBLE WITH DEPLOYMENT
+ */
+
+#if ENABLE_GPU_ACCELERATION && (FLOAT_PRECISION == 4)
+    #error "A quad floating-point precision (FLOAT_PRECISION=4, i.e. long double) is not supported by GPU deployment"
+#endif
+
+// Windows MSVC OpenMP doesn't permit operator overloading of the qcomp type,
+// as is necessary when performing multithreaded reductions of amplitudes.
+// We could support MSVC by separately reducing the real and imaginary components,
+// but Bill Gates would have to wrestle me into submission.
+#if ENABLE_MULTITHREADING && defined(_MSC_VER)
+    #error "Cannot use multi-threading on Windows"
+#endif
+
+
+
+/*
+ * MACROS FOR PRINTING MULTI-WORD MACROS
+ */
+
+#define GET_STR_INTERNAL(x) #x
+#define GET_STR(x) GET_STR_INTERNAL(x)
+
+
+
+/*
+ * RE-CONFIGURABLE VALIDATION PRECISION
+ */
+
+#ifndef VALIDATION_EPSILON
+
+    #if (FLOAT_PRECISION == 1)
+        #define VALIDATION_EPSILON 1E-5
+
+    #elif (FLOAT_PRECISION == 2)
+        #define VALIDATION_EPSILON 1E-13
+
+    #elif (FLOAT_PRECISION == 4)
+        #define VALIDATION_EPSILON 1E-14
+
+    #endif
+
+#endif
+
 
 
 #endif // PRECISION_H
--- quest/src/cpu/omp_subroutines.cpp
+++ quest/src/cpu/omp_subroutines.cpp
@@ -2,3 +2,16 @@
  * CPU OpenMP-accelerated definitions of the subroutines called by
  * accelerator.cpp. 
  */
+
+#include "quest/include/modes.h"
+#include "quest/include/types.h"
+
+#if ENABLE_MULTITHREADING
+    #include <omp.h>
+#endif
+
+
+// inform OpenMP how to reduce qcomp instances (except on MSVC compilers)
+#if defined(ENABLE_MULTITHREADING) && !defined(_MSC_VER)
+     #pragma omp declare reduction(+ : qcomp : omp_out += omp_in ) initializer( omp_priv = omp_orig )
+#endif
