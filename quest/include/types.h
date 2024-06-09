3dd9b08b0cfb2334ba5912f59f71da6104707374
TysonRayJones <tyson.jones.input@gmail.com>
2024-06-09 10:59:13 +1000

    added scalar types
    - `qreal`
    - `qindex`
    - `qcomp`
    
    The latter is a C/C++ agnostic complex scalar compatible with OpenMP, MPI, Thrust, cuQuantum, and given a CUDA-compatible wrapper

--- quest/include/types.h
+++ quest/include/types.h
@@ -5,6 +5,92 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include "quest/include/modes.h"
+#include "quest/include/precision.h"
+
+
+
+/*
+ * REAL TYPE ALIASES
+ */
+
+typedef FLOAT_TYPE qreal;
+typedef INDEX_TYPE qindex;
+
+
+
+/*
+ * COMPLEX TYPE ALIAS
+ */
+
+// when C++ parses this header during backend or C++ user-code compilation...
+#ifdef __cplusplus
+
+    // resolve qcomp as the standard C++ complex type
+    #include <complex>
+    typedef std::complex<FLOAT_TYPE> qcomp;
+
+// when C parses this header, during compilation of C user code...
+#else
+
+    // pretend that the API's qcomp is the C complex type
+    #include <complex.h>
+
+    // which is either MSVC's custom C complex...
+    #ifdef _MSC_VER
+
+        #if (FLOAT_PRECISION == 1)
+            typedef _Fcomplex qcomp;
+
+        #elif (FLOAT_PRECISION == 2)
+            typedef _Dcomplex qcomp;
+
+        #elif (FLOAT_PRECISION == 4)
+            typedef _Lcomplex qcomp;
+
+        #endif
+
+    // or that used by GNU & Clang
+    #else
+        typedef FLOAT_TYPE _Complex qcomp;
+
+    #endif
+
+#endif
+
+
+
+/*
+ * COMPLEX TYPE OVERLOADS
+ */
+
+#ifdef __cplusplus
+
+    // enable C++ literals (requires C++14)
+    using namespace std::complex_literals;
+
+    // qcomp() C++ instantiation is already enabled
+
+#else
+
+    #ifdef _MSC_VER
+
+        // MSVC C literals are literally impossible
+
+        // enable qcomp() C instantiation
+        #define qcomp(re,im) = (qcomp) {(re), (im)}
+
+    #else
+
+        // C literals are already enabled (requires C99)
+
+        // enable qcomp() C instantiation
+        #define qcomp(re,im) ( (qreal) (re) + I*((qreal) (im)) )
+
+    #endif
+
+#endif
+
 
 
 #endif // TYPES_H
--- quest/src/comm/communication.cpp
+++ quest/src/comm/communication.cpp
@@ -2,3 +2,35 @@
  * Functions for communicating and exchanging amplitudes between compute
  * nodes, when running in distributed mode, using the C MPI standard.
  */
+
+#include "quest/include/modes.h"
+#include "quest/include/types.h"
+
+#if ENABLE_DISTRIBUTION
+    #include <mpi.h>
+#endif
+
+
+
+/*
+ * MPI COMPLEX TYPE FLAG
+ */
+
+#if ENABLE_DISTRIBUTION
+
+    #if (FLOAT_PRECISION == 1)
+        #define MPI_QCOMP MPI_CXX_FLOAT_COMPLEX
+
+    #elif (FLOAT_PRECISION == 2)
+        #define MPI_QCOMP MPI_CXX_DOUBLE_COMPLEX
+
+    // sometimes 'MPI_CXX_LONG_DOUBLE_COMPLEX' isn't defined
+    #elif (FLOAT_PRECISION == 4) && defined(MPI_CXX_LONG_DOUBLE_COMPLEX)
+        #define MPI_QCOMP MPI_CXX_LONG_DOUBLE_COMPLEX
+
+    // in that case, fall back to the C type (identical memory layout)
+    #else
+        #define MPI_QCOMP MPI_C_LONG_DOUBLE_COMPLEX
+    #endif
+
+#endif
