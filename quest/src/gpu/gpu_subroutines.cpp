9fdf1f3129e17a255a814b98640617547d8c8454
Tyson Jones <tyson.jones.input@gmail.com>
2024-06-16 11:09:54 +0200

    scaffolded CUDA kernels

--- quest/src/gpu/gpu_subroutines.cpp
+++ quest/src/gpu/gpu_subroutines.cpp
@@ -8,3 +8,54 @@
  * When compiling for AMD GPUs, the CUDA symbols invoked herein are
  * mapped to HIP symbols by cuda_to_hip.h
  */
+
+#include "quest/include/modes.h"
+#include "quest/include/types.h"
+#include "quest/include/qureg.h"
+#include "quest/include/structures.h"
+
+#include "quest/src/core/errors.hpp"
+
+#if ENABLE_GPU_ACCELERATION
+    #include "quest/src/gpu/gpu_types.hpp"
+    #include "quest/src/gpu/gpu_kernels.hpp"
+#endif
+
+
+
+void gpu_statevec_oneTargetGate_subA(Qureg qureg, int target, CompMatr1 matrix) {
+#if ENABLE_GPU_ACCELERATION
+    
+    qindex numIts = qureg.numAmpsPerNode / 2;
+    qindex numBlocks = getNumBlocks(numIts);
+
+    // unpack matrix from qcomp into cu_qcomp
+    cu_qcomp m00,m01,m10,m11;
+    m00 = toCuQcomp(matrix.elems[0][0]);
+    m01 = toCuQcomp(matrix.elems[0][1]);
+    m10 = toCuQcomp(matrix.elems[1][0]);
+    m11 = toCuQcomp(matrix.elems[1][1]);
+
+    kernel_statevec_oneTargetGate_subA<<<numBlocks, NUM_THREADS_PER_BLOCK>>>(
+        toCuQcomps(qureg.gpuAmps), numIts, target, m00,m01,m10,m11);
+
+#else
+    error_gpuSimButGpuNotCompiled();
+#endif
+}
+
+void gpu_statevec_oneTargetGate_subB(Qureg qureg, qcomp fac0, qcomp fac1) {
+#if ENABLE_GPU_ACCELERATION
+    
+    qindex numIts = qureg.numAmpsPerNode;
+    qindex numBlocks = getNumBlocks(numIts);
+    
+    kernel_statevec_oneTargetGate_subB<<<numBlocks, NUM_THREADS_PER_BLOCK>>>(
+        toCuQcomps(qureg.gpuAmps), 
+        toCuQcomps(qureg.gpuCommBuffer), 
+        numIts, toCuQcomp(fac0), toCuQcomp(fac1));
+
+#else
+    error_gpuSimButGpuNotCompiled();
+#endif
+}
