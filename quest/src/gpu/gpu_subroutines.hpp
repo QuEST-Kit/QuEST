aa282a616cfb79dbf98b5e9d19e5b12bad936ec0
Tyson Jones <tyson.jones.input@gmail.com>
2024-06-16 11:09:54 +0200

    scaffolded CUDA kernels

--- quest/src/gpu/gpu_subroutines.hpp
+++ quest/src/gpu/gpu_subroutines.hpp
@@ -5,6 +5,15 @@
 #ifndef GPU_SUBROUTINES_HPP
 #define GPU_SUBROUTINES_HPP
 
+#include "quest/include/qureg.h"
+#include "quest/include/structures.h"
+
+
+
+void gpu_statevec_oneTargetGate_subA(Qureg qureg, int target, CompMatr1 matrix);
+
+void gpu_statevec_oneTargetGate_subB(Qureg qureg, qcomp fac0, qcomp fac1);
+
 
 
 #endif // GPU_SUBROUTINES_HPP
