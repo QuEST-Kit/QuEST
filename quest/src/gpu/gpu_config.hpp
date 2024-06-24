dbdb41c80429b81f72b3d5fe50f357912a38480c
TysonRayJones <tyson.jones.input@gmail.com>
2024-06-24 19:36:31 +0200

    oops

--- quest/src/gpu/gpu_config.hpp
+++ quest/src/gpu/gpu_config.hpp
@@ -20,6 +20,8 @@
 
 #if ENABLE_GPU_ACCELERATION
 
+#include <cuda.h>
+
 #define CUDA_CHECK(cmd) do { assertCudaCallSucceeded((cmd), #cmd, __FUNC__, __FILE__, __LINE__); } while (0)
 
 void assertCudaCallSucceeded(cudaError_t result, const char* call, const char* caller, const char* file, int line);
