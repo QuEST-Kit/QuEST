b15831a8a7b6298f4e8299b75d744e0912843eeb
TysonRayJones <tyson.jones.input@gmail.com>
2024-07-10 23:18:31 +0200

    prevent duplication when distirbuted

--- quest/src/api/types.cpp
+++ quest/src/api/types.cpp
@@ -5,11 +5,16 @@
 #include "quest/include/types.h"
 
 #include "quest/src/core/formatter.hpp"
+#include "quest/src/comm/comm_config.hpp"
 
 #include <iostream>
 
 
 extern "C" void reportQcomp(qcomp num) {
+
+    // only root node reports (but no synch necesary)
+    if (comm_getRank() != 0)
+        return;
 
     std::cout << form_str(num) << std::endl;
 }
