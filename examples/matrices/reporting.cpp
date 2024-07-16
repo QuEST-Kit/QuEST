0d935582df618265a821fc3f38506972f21e79fe
TysonRayJones <tyson.jones.input@gmail.com>
2024-07-16 15:36:13 +0200

    added examples

---
+++ examples/matrices/reporting.cpp
@@ -0,0 +1,88 @@
+#include "quest.h"
+#include <iostream>
+
+
+
+/*
+ * This demo is most interesting when run distributed with up to 64 nodes
+ */
+
+
+
+void rootPrint(qindex num) {
+
+    if (getQuESTEnv().rank != 0)
+        return;
+
+    std::cout << "\n\n>> set number of reported elements to " << num;
+
+    if (num == 0)
+        std::cout << " (which means report all)";
+         
+    std::cout << "\n" << std::endl; // flush
+}
+
+
+
+void demo_CompMatr() {
+
+    CompMatr matr = createCompMatr(4);
+    for (qindex r=0; r<matr.numRows; r++)
+        for (qindex c=0; c<matr.numRows; c++)
+            matr.cpuElems[r][c] = r*matr.numRows + c + 1;
+    syncCompMatr(matr);
+
+    for (int num : {0, 12, 5, 1}) {
+        rootPrint(num);
+        setNumReportedMatrixElems(num);
+        reportCompMatr(matr);
+    }
+}
+
+
+
+void demo_DiagMatr() {
+
+    DiagMatr matr = createDiagMatr(5);
+    for (qindex i=0; i<matr.numElems; i++)
+        matr.cpuElems[i] = i + 1;
+    syncDiagMatr(matr);
+
+    for (int num : {0, 10}) {
+        rootPrint(num);
+        setNumReportedMatrixElems(num);
+        reportDiagMatr(matr);
+    }
+}
+
+
+
+void demo_FullStateDiagMatr() {
+
+    // force distribution to make reporting interesting
+    FullStateDiagMatr matr = createCustomFullStateDiagMatr(6, 1);
+
+    // lazily set each element individually
+    for (qindex i=0; i<matr.numElems; i++)
+        setInlineFullStateDiagMatr(matr, i, 1, {i+1});
+
+    for (int num : {0, 50, 30, 10, 5, 1}) {
+        rootPrint(num);
+        setNumReportedMatrixElems(num);
+        reportFullStateDiagMatr(matr);
+    }
+}
+
+
+
+int main() {
+
+    initQuESTEnv();
+
+    demo_CompMatr();
+    demo_DiagMatr();
+    demo_FullStateDiagMatr();
+
+    finalizeQuESTEnv();
+    return 0;
+}
