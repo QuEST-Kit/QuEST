60e8a980e3d2826bf855bbe4189b61d6da86eeb3
Tyson Jones <tyson.jones.input@gmail.com>
2024-09-29 18:28:11 +0200

    added v4 signatures

--- quest/include/qureg.h
+++ quest/include/qureg.h
@@ -55,15 +55,53 @@ Qureg createDensityQureg(int numQubits);
 
 Qureg createCustomQureg(int numQubits, int isDensMatr, int useDistrib, int useGpuAccel, int useMultithread);
 
+Qureg createCloneQureg(Qureg qureg);
+
 void destroyQureg(Qureg qureg);
 
+void reportQuregParams(Qureg qureg);
 void reportQureg(Qureg qureg);
+void reportQuregToFile(Qureg qureg, char* fn);
 
+void syncQuregToGpu  (Qureg qureg);
+void syncQuregFromGpu(Qureg qureg);
+
+void syncSubQuregToGpu  (Qureg qureg, qindex startInd, qindex numAmps);
+void syncSubQuregFromGpu(Qureg qureg, qindex startInd, qindex numAmps);
+
+void syncSubDensityQuregToGpu  (Qureg qureg, qindex startRow, qindex startCol, qindex numRows, qindex numCols);
+void syncSubDensityQuregFromGpu(Qureg qureg, qindex startRow, qindex startCol, qindex numRows, qindex numCols);
 
 
 // end de-mangler
 #ifdef __cplusplus
 }
 #endif
+
+
+
+/*
+ * C++ ONLY FUNCTIONS
+ *
+ * which are not directly C-compatible because they pass or
+ * return qcomp primitives by-value (rather than by pointer).
+ * This is prohibited because the C and C++ ABI does not agree
+ * on a complex type, though C's _Complex has the same memory
+ * layout as C++'s std::complex<>. To work around this, the 
+ * below functions have a C-compatible wrapper defined in
+ * wrappers.h which passes/receives the primitives by pointer;
+ * a qcomp ptr can be safely passed from the C++ source binary
+ * the user's C binary.
+ */
+
+#ifdef __cplusplus
+
+qcomp getQuregAmp(Qureg qureg, qindex index);
+
+qcomp getDensityQuregAmp(Qureg qureg, qindex row, qindex column);
+
+#endif // __cplusplus
+
+
 
 #endif // QUREG_H
