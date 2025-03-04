a79b443d38475ce9c3e6f333db788e626171f5ed
Tyson Jones <tyson.jones.input@gmail.com>
2025-03-04 23:04:24 +0100

    worth a try

--- quest/src/gpu/gpu_thrust.cuh
+++ quest/src/gpu/gpu_thrust.cuh
@@ -47,6 +47,7 @@
 #include <thrust/transform_reduce.h>
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/iterator/transform_iterator.h>
+#include <thrust/system/system_error.h>
 
 
 
