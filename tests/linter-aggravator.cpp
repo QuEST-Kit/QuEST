19b3a081dd1bc2c2a58340dc21dd2a7bc4f28012
Tyson Jones <tyson.jones.input@gmail.com>
2025-03-05 23:59:09 +0100

    aggravating linter

---
+++ tests/linter-aggravator.cpp
@@ -0,0 +1,10 @@
+#include "quest.h"
+
+int main() {
+
+    // frankly, the existing source code is 
+    // likely stanky enough that the linter 
+    // will lose its mind; I flatter myself
+    // to imply I must deliberately provoke it.
+    return 0;
+}
