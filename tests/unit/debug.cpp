7fbe438a829f841c445ca9694068d30cb310e74a
Tyson Jones <tyson.jones.input@gmail.com>
2025-03-11 16:56:08 +0100

    adding missing test files

---
+++ tests/unit/debug.cpp
@@ -0,0 +1,65 @@
+/** @file
+ * Unit tests of the debug module.
+ *
+ * @author Tyson Jones
+ * 
+ * @defgroup unitdebug Debug unit tests
+ * @ingroup unittests
+ */
+
+#include "quest/include/quest.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+
+
+/*
+ * UTILITIES
+ */
+
+#define TEST_CATEGORY "[unit][debug]"
+
+
+
+/** 
+ * TESTS
+ * 
+ * @ingroup unitdebug
+ * @{
+ */
+ 
+TEST_CASE( "placeholder", TEST_CATEGORY) {
+ 
+}
+ 
+/** @} (end defgroup) */
+
+
+
+/*
+ * @todo
+ * UNTESTED FUNCTIONS
+ */
+
+void setSeeds(unsigned* seeds, int numSeeds);
+void setSeedsToDefault();
+
+void getSeeds(unsigned* seeds);
+int getNumSeeds();
+
+// void invalidQuESTInputError(const char* msg, const char* func);
+
+void setValidationOn();
+void setValidationOff();
+
+void setValidationEpsilonToDefault();
+void setValidationEpsilon(qreal eps);
+qreal getValidationEpsilon();
+
+void setMaxNumReportedItems(qindex numRows, qindex numCols);
+void setMaxNumReportedSigFigs(int numSigFigs);
+
+qindex getGpuCacheSize();
+void clearGpuCache();
+
+void getEnvironmentString(char str[200]);
