1fc6ecfe9da8b0b11df833546870643d5529a6d1
Tyson Jones <tyson.jones.input@gmail.com>
2025-05-06 00:05:25 +0200

    added calcRealAmpSum tests

--- tests/unit/calculations.cpp
+++ tests/unit/calculations.cpp
@@ -156,6 +156,47 @@ void TEST_ON_CACHED_QUREG_AND_MATRIX(quregCache quregs, matrixCache matrices, au
 
 
 
+TEST_CASE( "calcRealAmpSum", TEST_CATEGORY ) {
+
+    SECTION( LABEL_CORRECTNESS ) {
+
+        // The boilerplate for testing a function differs
+        // greatly depending on what the function does;
+        // this function is trivial so has a simple test,
+        // re-using the existing TEST_ALL_QUREGS() macro.
+        // This macro invokes the below RHS expressions
+        // passing substitutions of the LHS expressions
+        // with Quregs (statevector or density matrix)
+        // and reference objects (qvector or qmatrix), for
+        // every possible deployment (i.e. multithreading,
+        // GPU-acceleration, distribution, hybrids, etc).
+
+        TEST_ALL_QUREGS(
+            qureg, calcRealAmpSum(qureg),
+            refer, std::real(getTotal(refer))
+        );
+    }
+
+    SECTION( LABEL_VALIDATION ) {
+
+        SECTION( "qureg uninitialised" ) {
+
+            // prepare an un-initialised qureg
+            Qureg qureg;
+
+            // manually mangle the fields for validation
+            // to detect, since the default values are
+            // undefined behaviour and might not trigger
+            // (e.g. compiler could re-use a valid Qureg)
+            qureg.numQubits = -123;
+
+            REQUIRE_THROWS_WITH( calcRealAmpSum(qureg), ContainsSubstring("invalid Qureg") );
+        }
+    }
+}
+
+
+
 TEST_CASE( "calcExpecPauliStr", TEST_CATEGORY ) {
 
     SECTION( LABEL_CORRECTNESS ) {
