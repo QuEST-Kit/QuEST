QuEST-Kit/QuEST

commit 9be1dfd4b7359c45fd7732d41ee611622cc695a1

patched CMake install (#616)
This resolves the issues highlighted in #611 while maintaining QuEST's header include structure in the source

CMakeLists.txt
@@ -67,6 +67,22 @@ message(STATUS "Library will be named lib${LIB_NAME}. Set LIB_NAME to modify.")
 option(VERBOSE_LIB_NAME "Modify library name based on compilation configuration. Turned OFF by default." OFF)
 message(STATUS "Verbose library naming is turned ${VERBOSE_LIB_NAME}. Set VERBOSE_LIB_NAME to modify.")
 
+if (VERBOSE_LIB_NAME)
+  # Same headers will be used for several verbosely-named libraries
+  set(MULTI_LIB_HEADERS 1)
+  function(compile_option VAR VALUE)
+    target_compile_definitions(QuEST PUBLIC ${VAR}=${VALUE})
+  endfunction()
+else()
+  # Headers will be used for a single library with a single valid configuration
+  set(MULTI_LIB_HEADERS 0)
+  function(compile_option VAR VALUE)
+    target_compile_definitions(QuEST PRIVATE ${VAR}=${VALUE})
+    set(${VAR} ${VALUE})
+  endfunction()
+endif()
+
+
 # Precision
 set(FLOAT_PRECISION 2 
   CACHE 
@@ -253,8 +269,8 @@ target_include_directories(QuEST
         PUBLIC
         $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/quest/include>
         $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/include>
         $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
-        $<INSTALL_INTERFACE:${ORG_INSTALL_PATH}>
 )
 set_target_properties(QuEST PROPERTIES
         VERSION     ${PROJECT_VERSION}
@@ -283,7 +299,7 @@ target_compile_options(QuEST
 
 
 # Set user options
-target_compile_definitions(QuEST PUBLIC FLOAT_PRECISION=${FLOAT_PRECISION})
+compile_option(FLOAT_PRECISION ${FLOAT_PRECISION})
 
 if (ENABLE_MULTITHREADING)
 
@@ -299,7 +315,7 @@ if (ENABLE_MULTITHREADING)
     message(FATAL_ERROR ${ErrorMsg})
   endif()
 
-  target_compile_definitions(QuEST PUBLIC COMPILE_OPENMP=1)
+  compile_option(COMPILE_OPENMP 1)
   target_link_libraries(QuEST
     PRIVATE
     OpenMP::OpenMP_CXX
@@ -317,14 +333,14 @@ else()
     target_compile_options(QuEST PRIVATE $<$<COMPILE_LANGUAGE:CXX>:-Wno-unknown-pragmas>)
   endif()
   
-  target_compile_definitions(QuEST PUBLIC COMPILE_OPENMP=0)
+  compile_option(COMPILE_OPENMP 0)
 endif()
 
 if (ENABLE_DISTRIBUTION)
   find_package(MPI REQUIRED
     COMPONENTS CXX
   )
-  target_compile_definitions(QuEST PUBLIC COMPILE_MPI=1)
+  compile_option(COMPILE_MPI 1)
   target_link_libraries(QuEST
     PRIVATE
     MPI::MPI_CXX
@@ -333,7 +349,7 @@ if (ENABLE_DISTRIBUTION)
     string(CONCAT LIB_NAME ${LIB_NAME} "+mpi")
   endif()
 else()
-  target_compile_definitions(QuEST PUBLIC COMPILE_MPI=0)
+  compile_option(COMPILE_MPI 0)
 endif()
 
 if (ENABLE_CUDA)
@@ -357,14 +373,14 @@ endif()
 
 if (ENABLE_CUQUANTUM)
   find_package(CUQUANTUM REQUIRED)
-  target_compile_definitions(QuEST PUBLIC COMPILE_CUQUANTUM=1)
+  compile_option(COMPILE_CUQUANTUM 1)
   target_link_libraries(QuEST PRIVATE CUQUANTUM::cuStateVec)
   set(CMAKE_INSTALL_RPATH_USE_LINK_PATH ON)
   if (VERBOSE_LIB_NAME)
     string(CONCAT LIB_NAME ${LIB_NAME} "+cuquantum")
   endif()
 else()
-  target_compile_definitions(QuEST PUBLIC COMPILE_CUQUANTUM=0)
+  compile_option(COMPILE_CUQUANTUM 0)
 endif()
 
 if (ENABLE_HIP)
@@ -384,7 +400,7 @@ if (ENABLE_HIP)
   find_package(HIP REQUIRED)
   message(STATUS "Found HIP: " ${HIP_VERSION})
 
-  target_compile_definitions(QuEST PUBLIC COMPILE_CUQUANTUM=0)
+  compile_option(COMPILE_CUQUANTUM 0)
   target_link_libraries(QuEST PRIVATE hip::host)
 
   if (VERBOSE_LIB_NAME)
@@ -393,9 +409,9 @@ if (ENABLE_HIP)
 endif()
 
 if (ENABLE_CUDA OR ENABLE_HIP)
-  target_compile_definitions(QuEST PUBLIC COMPILE_CUDA=1)
+  compile_option(COMPILE_CUDA 1)
 else()
-  target_compile_definitions(QuEST PUBLIC COMPILE_CUDA=0)
+  compile_option(COMPILE_CUDA 0)
 endif()
 
 if (ENABLE_DEPRECATED_API)
@@ -533,7 +549,7 @@ set(QuEST_INSTALL_CONFIGDIR "${CMAKE_INSTALL_LIBDIR}/cmake/QuEST")
 
 # Write QuESTConfigVersion.cmake
 write_basic_package_version_file(
-        "${CMAKE_CURRENT_BINARY_DIR}/QuESTConfigVersion.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/${LIB_NAME}ConfigVersion.cmake"
         VERSION ${PROJECT_VERSION}
         COMPATIBILITY AnyNewerVersion
 )
@@ -541,19 +557,28 @@ write_basic_package_version_file(
 # Configure QuESTConfig.cmake (from template)
 configure_package_config_file(
         "${CMAKE_CURRENT_SOURCE_DIR}/cmake/QuESTConfig.cmake.in"
-        "${CMAKE_CURRENT_BINARY_DIR}/QuESTConfig.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/${LIB_NAME}Config.cmake"
         INSTALL_DESTINATION "${QuEST_INSTALL_CONFIGDIR}"
 )
 
 # Install them
 install(FILES
-        "${CMAKE_CURRENT_BINARY_DIR}/QuESTConfig.cmake"
-        "${CMAKE_CURRENT_BINARY_DIR}/QuESTConfigVersion.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/${LIB_NAME}Config.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/${LIB_NAME}ConfigVersion.cmake"
         DESTINATION "${QuEST_INSTALL_CONFIGDIR}"
 )
 
+install(FILES "${CMAKE_CURRENT_BINARY_DIR}/include/quest.h"
+        DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+)
+
+install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/quest/include"
+        DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/quest"
+        FILES_MATCHING PATTERN "*.h"
+)
+
 install(EXPORT QuESTTargets
-        FILE QuESTTargets.cmake
+        FILE "${LIB_NAME}Targets.cmake"
         NAMESPACE QuEST::
         DESTINATION "${QuEST_INSTALL_CONFIGDIR}"
 )

quest/include/CMakeLists.txt
@@ -1,7 +1,4 @@
 # @author Oliver Thomson Brown
 # @author Erich Essmann
 
-install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        DESTINATION "${CMAKE_INSTALL_PREFIX}"
-        FILES_MATCHING PATTERN "*.h" 
-)
+configure_file(quest.h.in "${CMAKE_BINARY_DIR}/include/quest.h" @ONLY)

cmake/QuESTConfig.cmake.in
@@ -1,4 +1,4 @@
 # @author Erich Essmann
 
 @PACKAGE_INIT@
-include("${CMAKE_CURRENT_LIST_DIR}/QuESTTargets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/@LIB_NAME@Targets.cmake")

quest/include/deprecated.h
@@ -18,7 +18,7 @@
 #ifndef DEPRECATED_H
 #define DEPRECATED_H
 
-#include "quest/include/quest.h"
+#include "quest.h"
 
 #include "stdlib.h"
 

quest/include/quest.h.in
@@ -30,6 +30,15 @@
 #define QUEST_H
 
 
+#if !@MULTI_LIB_HEADERS@
+#cmakedefine FLOAT_PRECISION @FLOAT_PRECISION@
+#cmakedefine01 COMPILE_MPI
+#cmakedefine01 COMPILE_OPENMP
+#cmakedefine01 COMPILE_CUDA
+#cmakedefine01 COMPILE_CUQUANTUM
+#endif
+
+
 // include version first so it is accessible to 
 // debuggers in case a subsequent include fails
 #include "quest/include/version.h"

tests/deprecated/test_calculations.cpp
@@ -20,7 +20,7 @@
 // warnings issued by its compilation
 #define INCLUDE_DEPRECATED_FUNCTIONS 1
 #define DISABLE_DEPRECATION_WARNINGS 1
-#include "quest/include/quest.h"
+#include "quest.h"
 
 #include "test_utilities.hpp"
 

tests/deprecated/test_data_structures.cpp
@@ -20,7 +20,7 @@
 // warnings issued by its compilation
 #define INCLUDE_DEPRECATED_FUNCTIONS 1
 #define DISABLE_DEPRECATION_WARNINGS 1
-#include "quest/include/quest.h"
+#include "quest.h"
 
 #include "test_utilities.hpp"
 

tests/deprecated/test_gates.cpp
@@ -23,7 +23,7 @@
 // warnings issued by its compilation
 #define INCLUDE_DEPRECATED_FUNCTIONS 1
 #define DISABLE_DEPRECATION_WARNINGS 1
-#include "quest/include/quest.h"
+#include "quest.h"
 
 #include "test_utilities.hpp"
 

commit 8678836cd29ac59688556399860aab712db1fe14

updating version number (#628)
and fixing doxygen parsing of CMake configured header broken in #616

CMakeLists.txt
@@ -16,7 +16,7 @@
 cmake_minimum_required(VERSION 3.21)
 
 project(QuEST
-  VERSION 4.0.0
+  VERSION 4.1.0
   DESCRIPTION "Quantum Exact Simulation Toolkit"
   LANGUAGES CXX C
 )

quest/include/debug.h
@@ -131,8 +131,8 @@ void setNumReportedNewlines(int numNewlines);
 
 
 /** 
- * @notdoced
- * @nottested
+ * @notyetdoced
+ * @notyettested
  * @myexample
  * ```
    PauliStr str = getInlinePauliStr("XYZ", {0,10,20});
@@ -146,8 +146,8 @@ void setReportedPauliChars(const char* paulis);
 
 
 /** 
- * @notdoced
- * @nottested
+ * @notyetdoced
+ * @notyettested
  * @myexample
  * ```
    PauliStr str = getInlinePauliStr("XYZ", {0,10,20});

quest/include/version.h
@@ -11,8 +11,8 @@
 
 // TODO: automate this from git somehow
 #define QUEST_VERSION_MAJOR 4
-#define QUEST_VERSION_MINOR 0
+#define QUEST_VERSION_MINOR 1
 #define QUEST_VERSION_PATCH 0
-#define QUEST_VERSION_STRING "4.0.0"
+#define QUEST_VERSION_STRING "4.1.0"
 
 #endif // VERSION_H

utils/docs/Doxyfile
@@ -51,7 +51,7 @@ PROJECT_NAME           = "The Quantum Exact Simulation Toolkit"
 # could be handy for archiving the generated documentation or if some version
 # control system is used.
 
-PROJECT_NUMBER         = "v4.0.0"
+PROJECT_NUMBER         = "v4.1.0"
 
 # Using the PROJECT_BRIEF tag one can provide an optional one line description
 # for a project that appears at the top of each page and should give viewer a
@@ -373,8 +373,9 @@ OPTIMIZE_OUTPUT_SLICE  = NO
 #
 # Note see also the list of default file extension mappings.
 
-# all source code (even headers) is C++, but example.c code is C
-EXTENSION_MAPPING      = h=C++ hpp=C++ cpp=C++ cu=C++ cuh=C++ c=C
+# all source code (even headers) is C++, but example.c code is C.
+# in=C is necessary to parse quest.h.in (a CMake configured header)
+EXTENSION_MAPPING      = h=C++ hpp=C++ cpp=C++ cu=C++ cuh=C++ c=C in=C
 
 # If the MARKDOWN_SUPPORT tag is enabled then doxygen pre-processes all comments
 # according to the Markdown format, which allows for more readable
@@ -1088,7 +1089,7 @@ INPUT_FILE_ENCODING    =
 # be provided as Doxygen C comment), *.py, *.pyw, *.f90, *.f95, *.f03, *.f08,
 # *.f18, *.f, *.for, *.vhd, *.vhdl, *.ucf, *.qsf and *.ice.
 
-FILE_PATTERNS          = *.c *.cpp *.cu *.cuh *.h *.hpp *.md *.sh
+FILE_PATTERNS          = *.c *.cpp *.cu *.cuh *.h *.h.in *.hpp *.md *.sh
 
 # The RECURSIVE tag can be used to specify whether or not subdirectories should
 # be searched for input files as well.