b08a50420b7ee7f1242ebd974ec6b0f6eb3a7fb7
Tyson Jones <tyson.jones.input@gmail.com>
2025-06-03 16:40:21 +0200

    updated test validation messages

--- tests/unit/channels.cpp
+++ tests/unit/channels.cpp
@@ -126,7 +126,11 @@ TEST_CASE( "createKrausMap", TEST_CATEGORY ) {
             // GPU-accel or distributed) and whether memory-probers realised there was insufficient memory in
             // advance or whether it proceeded to malloc() which subsequently failed
             #ifndef SANITIZER_IS_ACTIVE
-            REQUIRE_THROWS_WITH( createKrausMap(12,1), ContainsSubstring("failed") || ContainsSubstring("insufficient available memory") || ContainsSubstring("available GPU memory") );
+            REQUIRE_THROWS_WITH( createKrausMap(12,1), 
+                ContainsSubstring("failed") || 
+                ContainsSubstring("insufficient available memory") || 
+                ContainsSubstring("exceeds the available memory") ||
+                ContainsSubstring("available GPU memory") );
             #endif
         }
 
@@ -512,7 +516,11 @@ TEST_CASE( "createSuperOp", TEST_CATEGORY ) {
             // GPU-accel or distributed) and whether memory-probers realised there was insufficient memory in
             // advance or whether it proceeded to malloc() which subsequently failed
             #ifndef SANITIZER_IS_ACTIVE
-            REQUIRE_THROWS_WITH( createSuperOp(12), ContainsSubstring("failed") || ContainsSubstring("insufficient available memory") || ContainsSubstring("available GPU memory") );
+            REQUIRE_THROWS_WITH( createSuperOp(12), 
+                ContainsSubstring("failed") || 
+                ContainsSubstring("insufficient available memory") || 
+                ContainsSubstring("available GPU memory") ||
+                ContainsSubstring("exceeds that available") );
             #endif
         }
     }
