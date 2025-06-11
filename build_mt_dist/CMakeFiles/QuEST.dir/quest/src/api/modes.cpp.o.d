dc94bb2b66ff6b57bbb65644f28ccfa44a5221e6
Tyson Jones <tyson.jones.input@gmail.com>
2025-06-11 12:06:03 +0200

    removed superfluous build_mt_dist folder

--- build_mt_dist/CMakeFiles/QuEST.dir/quest/src/api/modes.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/QuEST.dir/quest/src/api/modes.cpp.o: \
- /workspaces/QuEST/quest/src/api/modes.cpp /usr/include/stdc-predef.h \
- /workspaces/QuEST/quest/include/modes.h
--- build_mt_dist/CMakeFiles/min_example.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for min_example.
-# This may be replaced when dependencies are built.
