2893253cfb9540cdf6b33c6491fd8f4c7c0bf3a7
Tyson Jones <tyson.jones.input@gmail.com>
2025-09-21 22:22:12 -0400

    added all calc-expec doc

--- quest/include/calculations.h
+++ quest/include/calculations.h
@@ -114,7 +114,7 @@ qreal calcExpecPauliStr(Qureg qureg, PauliStr str);
  *   @f[ 
      \tr{ \hat{H} \dmrho }
  *   @f]
- *   which is the exact expectation value when @f$\dmrho@f$ is physical (specifically Hermitian).
+ *   which is the exact expectation value when @f$\dmrho@f$ is physical (or at least, Hermitian).
  * 
  * @constraints
  * - Hermiticity of @p sum requires that every coefficient within is real. 
@@ -165,14 +165,161 @@ qreal calcExpecPauliStr(Qureg qureg, PauliStr str);
 qreal calcExpecPauliStrSum(Qureg qureg, PauliStrSum sum);
 
 
-/// @notyetdoced
-/// @notyetvalidated
+/** Calculates the expectation value of the given Hermitian observable @p matr - a diagonal,
+ * Hermitian matrix spanning the full Hilbert space - under the given state @p qureg, without 
+ * modifying it. 
+ * 
+ * @formulae
+ * Let @f$ \hat{D} = @f$ @p matr.
+ * - When @p qureg is a statevector @f$\svpsi@f$, this function returns
+ *   @f[ 
+    \brapsi \hat{D} \svpsi \in \mathbb{R}.
+ *   @f]
+ * - When @p qureg is a density matrix @f$\dmrho@f$, this function returns the real component of
+ *   @f[ 
+     \tr{ \hat{D} \dmrho }
+ *   @f]
+ *   which is the exact expectation value when @f$\dmrho@f$ is physical (or at least, Hermitian).
+ * 
+ * @constraints
+ * - Hermiticity of @p matr requires that every element within is real. 
+ *   Validation will check @p matr is _approximately_ Hermitian, i.e. that
+ *   @f[ 
+     |\im{c}| \le \valeps
+ *   @f]
+ *   for all @f$c \in @f$ `matr.cpuElems`. Adjust @f$\valeps@f$ using setValidationEpsilon().
+ * - The returned value is always real, and the imaginary component is neglected even when 
+ *   Hermiticity validation is relaxed and/or @p qureg is an unnormalised density matrix. 
+ *   The full complex value can be obtained using calcExpecNonHermitianFullStateDiagMatr().
+ * 
+ * @equivalences
+ * - This function is mathematically equivalent to (albeit much faster than) calling calcExpecPauliStrSum()
+ *   with a PauliStrSum consisting of all permutations of @f$\hat{I}@f$ and @f$\hat{Z}@f$ Pauli operators
+ *   with a precise, linear combination of coefficients.
+ * 
+ * @myexample
+ * ```
+    Qureg qureg = createQureg(5);
+    initPlusState(qureg);
+
+    FullStateDiagMatr matr = createFullStateDiagMatr(qureg.numQubits);
+
+    // profanely inefficient per-element initialisation
+    for (int n=0; n<matr.numElems; n++) {
+        qcomp elem = getQcomp(n, 0);
+        setFullStateDiagMatr(matr, n, &elem, 1);
+    }
+
+    // prints "expec: 15.5"
+    qreal expec = calcExpecFullStateDiagMatr(qureg, matr);
+    reportScalar("expec", expec);
+ * ```
+ *
+ * @param[in] qureg the reference state.
+ * @param[in] matr  the observable operator.
+ * @returns The real component of the expectation value.
+ * @throws @validationerror
+ * - if @p qureg or @p matr are uninitialised.
+ * - if @p matr does not match the dimension of @p qureg
+ * - if @p matr is distributed but @p qureg is not
+ * - if @p matr is not approximately Hermitian.
+ * - if the output (with unreturned imaginary component) is not approximately real.
+* @notyetvalidated
+ * @see
+ * - calcExpecFullStateDiagMatrPower()
+ * - calcExpecNonHermitianFullStateDiagMatr()
+ * - calcExpecPauliStrSum()
+ * @author Tyson Jones
+ */
 qreal calcExpecFullStateDiagMatr(Qureg qureg, FullStateDiagMatr matr);
 
 
-/// @notyetdoced
-/// @notyetvalidated
-qreal calcExpecFullStateDiagMatrPower(Qureg qureg, FullStateDiagMatr matr, qreal exponent);
+/** Calculates the expectation value of the given Hermitian observable @p matrix - a diagonal,
+ * Hermitian matrix spanning the full Hilbert space - when raised to the given @p exponent,
+ * under the given state @p qureg, which is not modified.
+ * 
+ * @formulae
+ * Let @f$ \hat{D} = @f$ @p matrix and @f$x = @f$ @p exponent.
+ * - When @p qureg is a statevector @f$\svpsi@f$, this function returns
+ *   @f[ 
+    \brapsi \hat{D}^x \svpsi \in \mathbb{R}.
+ *   @f]
+ * - When @p qureg is a density matrix @f$\dmrho@f$, this function returns the real component of
+ *   @f[ 
+     \tr{ \hat{D}^x \dmrho }
+ *   @f]
+ *   which is the exact expectation value when @f$\dmrho@f$ is physical (or at least, Hermitian).
+ * 
+ * @constraints
+ * - Hermiticity of @p matrix itself requires that every element within is real. 
+ *   Validation will check @p matrix is _approximately_ Hermitian, i.e. that
+ *   @f[ 
+     |\im{c}| \le \valeps
+ *   @f]
+ *   for all @f$c \in @f$ `matr.cpuElems`. Adjust @f$\valeps@f$ using setValidationEpsilon().
+ * 
+ *   > [!CAUTION]
+ *   > Unlike other functions (including calcExpecFullStateDiagMatr()), this function will _NOT_
+ *   > consult the imaginary components of the elements of @p matrix, since a non-complex exponentiation
+ *   > function is used. That is, while validation permits the imaginary components to be small, they
+ *   > will be internally treated as precisely zero. This is true even when Hermiticity validation
+ *   > is disabled using setValidationOff(). To consult the imaginary components of @p matrix, use
+ *   > calcExpecNonHermitianFullStateDiagMatrPower().
+ * 
+ * - Hermiticity of @p matrix when raised to @p exponent further requires that, when @p exponent is 
+ *   a non-integer, @p matrix does not contain any negative elements which would otherwise produce 
+ *   complex elements in @f$\hat{D}^x@f$. This validation is always strict (i.e. independent of 
+ *   @f$\valeps@f$), and demands that
+ *   @f[ 
+     \min(\hat{D}) \ge 0 \text{ when } x \notin \mathbb{R}.
+ *   @f]
+ * - Numerical stability requires that if @p exponent is negative, @p matrix does not contain any
+ *   zero elements which would otherwise create divergences in @f$\hat{D}^x@f$. Validation ergo
+ *   checks that when @p exponent is (strictly) negative, @p matrix contains no elements within 
+ *   distance @f$\valeps@f$ to zero (regardless of the magnitude of @p exponent). Adjust
+ *   @f$\valeps@f$ using setValidationEpsilon().
+ * - The passed @p exponent is always real, but can be relaxed to a general complex scalar via
+ *   calcExpecNonHermitianFullStateDiagMatrPower().
+ * - The returned value is always real, and the imaginary component is neglected even when 
+ *   Hermiticity validation is relaxed and/or @p qureg is an unnormalised density matrix. 
+ *   The full complex value can be obtained using calcExpecNonHermitianFullStateDiagMatrPower().
+ * 
+ * @myexample
+ * ```
+    Qureg qureg = createQureg(5);
+    initPlusState(qureg);
+
+    FullStateDiagMatr matrix = createFullStateDiagMatr(qureg.numQubits);
+
+    // profanely inefficient per-element initialisation
+    for (int n=0; n<matrix.numElems; n++) {
+        qcomp elem = getQcomp(n+1, 0);
+        setFullStateDiagMatr(matrix, n, &elem, 1);
+    }
+
+    // prints "expec: 0.044503"
+    qreal exponent = -2.3;
+    qreal expec = calcExpecFullStateDiagMatrPower(qureg, matrix, exponent);
+    reportScalar("expec", expec);
+ * ```
+ * @param[in] qureg     the reference state.
+ * @param[in] matrix    the observable operator.
+ * @param[in] exponent  the exponent to which to raise @p matrix
+ * @returns The real component of the expectation value of @p matrix raised to @p exponent.
+ * @throws @validationerror
+ * - if @p qureg or @p matrix are uninitialised.
+ * - if @p matrix does not match the dimension of @p qureg
+ * - if @p matrix is distributed but @p qureg is not
+ * - if @p matrix is not approximately Hermitian.
+ * - if @p exponent is (precisely) non-integer but @p matrix contains (precisely) negative elements.
+ * - if @p exponent is (precisely) negative but @p matrix contains elements which are approximately zero. 
+ * - if the output (with unreturned imaginary component) is not approximately real.
+ * @notyetvalidated
+ * @see
+ * - calcExpecNonHermitianFullStateDiagMatrPower()
+ * @author Tyson Jones
+ */
+qreal calcExpecFullStateDiagMatrPower(Qureg qureg, FullStateDiagMatr matrix, qreal exponent);
 
 
 /** @} */
@@ -300,21 +447,137 @@ Qureg calcReducedDensityMatrix(Qureg qureg, int* retainQubits, int numRetainQubi
 qcomp calcInnerProduct(Qureg qureg1, Qureg qureg2);
 
 
-/// @ingroup calc_expec
-/// @notyetdoced
-/// @notyetvalidated
+/** @ingroup calc_expec
+ * 
+ * Calculates the expectation value of the given permittedly non-Hermitian operator @p sum 
+ * - a weighted sum of Pauli strings with complex weights - under the given state @p qureg, 
+ * which is not modified.
+ * 
+ * @formulae
+ * 
+ * This function is mathematically equivalent to calcExpecPauliStrSum(), _except_ that here a
+ * complex scalar is returned. This permits obtaining the full scalar when @p sum contains non-real
+ * weights, and/or when @p qureg is unnormalised.
+ *
+ * @myexample
+ * ```
+    Qureg qureg = createQureg(5);
+    PauliStrSum sum = createInlinePauliStrSum(R"(
+        0.123 + 3.5i  ZIZIZI
+        1.234 - 1E-5i XYZXZ
+        -1E-2         IIIII
+    )");
+
+    // prints "expec: 0.113+3.5i"
+    qcomp expec = calcExpecNonHermitianPauliStrSum(qureg, sum);
+    reportScalar("expec", expec);  
+ * ```
+ *
+ * @param[in] qureg the permittedly unnormalised reference state.
+ * @param[in] sum   the permittedly non-Hermitian operator.
+ * @returns The permittedly complex expectation value.
+ * @throws @validationerror
+ * - if @p qureg or @p sum are uninitialised.
+ * - if any PauliStr in @p sum targets a higher-index qubit than exists in @p qureg.
+* @notyetvalidated
+ * @see
+ * - calcExpecPauliStrSum()
+ * @author Tyson Jones
+ */
 qcomp calcExpecNonHermitianPauliStrSum(Qureg qureg, PauliStrSum sum); 
 
 
-/// @ingroup calc_expec
-/// @notyetdoced
-/// @notyetvalidated
+/** @ingroup calc_expec
+ * 
+ * Calculates the expectation value of the given permittedly non-Hermitian operator @p matr,
+ * under the given state @p qureg, without modifying it. 
+ * 
+ * @formulae
+ * 
+ * This function is mathematically equivalent to calcExpecFullStateDiagMatr(), _except_ that here a
+ * complex scalar is returned. This permits obtaining the full scalar when @p sum contains non-real
+ * elements, and/or when @p qureg is unnormalised.
+ *
+ * @myexample
+ * ```
+    Qureg qureg = createQureg(5);
+    initPlusState(qureg);
+
+    FullStateDiagMatr matr = createFullStateDiagMatr(qureg.numQubits);
+
+    // profanely inefficient per-element initialisation
+    for (int n=0; n<matr.numElems; n++) {
+        qcomp elem = getQcomp(n, n+1);
+        setFullStateDiagMatr(matr, n, &elem, 1);
+    }
+
+    // prints "expec: 15.5+16.5i"
+    qcomp expec = calcExpecNonHermitianFullStateDiagMatr(qureg, matr);
+    reportScalar("expec", expec);
+ * ```
+ *
+ * @param[in] qureg the permittedly unnormalised reference state.
+ * @param[in] matr  the permittedly non-Hermitian operator.
+ * @returns The permittedly complex expectation value.
+ * @throws @validationerror
+ * - if @p qureg or @p matr are uninitialised.
+ * - if @p matr does not match the dimension of @p qureg
+ * - if @p matr is distributed but @p qureg is not
+* @notyetvalidated
+ * @see
+ * - calcExpecFullStateDiagMatr()
+ * - calcExpecFullStateDiagMatrPower()
+ * - calcExpecNonHermitianFullStateDiagMatrPower()
+ * @author Tyson Jones
+ */
 qcomp calcExpecNonHermitianFullStateDiagMatr(Qureg qureg, FullStateDiagMatr matr);
 
 
-/// @ingroup calc_expec
-/// @notyetdoced
-/// @notyetvalidated
+/** @ingroup calc_expec
+ * 
+ * Calculates the expectation value of the given permittedly non-Hermitian operator @p matrix,
+ * raised to the arbitrary complex @p exponent, under the given state @p qureg, which is not modified.
+ * 
+ * @formulae
+ * 
+ * This function is mathematically equivalent to calcExpecFullStateDiagMatrPower(), _except_ that 
+ * here a complex scalar is returned, in addition to @p exponent being permittedly complex.
+ * This permits obtaining the full scalar when @p qureg is unnormalised or @p matrix (after being
+ * raised to @p exponent) is non-Hermitian.
+ *
+ * @myexample
+ * ```
+    Qureg qureg = createQureg(5);
+    initPlusState(qureg);
+
+    FullStateDiagMatr matrix = createFullStateDiagMatr(qureg.numQubits);
+
+    // profanely inefficient per-element initialisation
+    for (int n=0; n<matrix.numElems; n++) {
+        qcomp elem = getQcomp(n, n+1);
+        setFullStateDiagMatr(matrix, n, &elem, 1);
+    }
+
+    qcomp exponent = 3+4_i;
+
+    // prints "expec: -257.26-613.8i"
+    qcomp expec = calcExpecNonHermitianFullStateDiagMatrPower(qureg, matrix, exponent);
+    reportScalar("expec", expec);
+ * ```
+ *
+ * @param[in] qureg     the permittedly unnormalised reference state.
+ * @param[in] matrix    the permittedly non-Hermitian operator.
+ * @param[in] exponent  the permittedly complex exponent.
+ * @returns The permittedly complex expectation value.
+ * @throws @validationerror
+ * - if @p qureg or @p matrix are uninitialised.
+ * - if @p matrix does not match the dimension of @p qureg
+ * - if @p matrix is distributed but @p qureg is not
+* @notyetvalidated
+ * @see
+ * - calcExpecFullStateDiagMatr()
+ * @author Tyson Jones
+ */
 qcomp calcExpecNonHermitianFullStateDiagMatrPower(Qureg qureg, FullStateDiagMatr matrix, qcomp exponent);
 
 
