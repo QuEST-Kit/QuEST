f7b0946da6e3711a166770c1d4deaa963929752b
LubuSeb <187313664+LubuSeb@users.noreply.github.com>
2026-06-05 17:12:14 +0200

    Improve applyCompMatr summation accuracy

--- quest/src/cpu/cpu_subroutines.cpp
+++ quest/src/cpu/cpu_subroutines.cpp
@@ -607,7 +607,8 @@ void cpu_statevec_anyCtrlAnyTargDenseMatr_sub(Qureg qureg, ConstList64 ctrls, Co
 
                 // i = nth local index where ctrls are active and targs form value k
                 qindex i = setBits(i0, targs.data(), numTargBits, k); // loop may be unrolled
-                amps[i] = getCpuQcomp(0, 0);
+                cpu_qcomp sum = getCpuQcomp(0, 0);
+                cpu_qcomp correction = getCpuQcomp(0, 0);
             
                 // loop may be unrolled
                 for (qindex j=0; j<numTargAmps; j++) {
@@ -624,18 +625,19 @@ void cpu_statevec_anyCtrlAnyTargDenseMatr_sub(Qureg qureg, ConstList64 ctrls, Co
                     if constexpr (ApplyConj)
                         elem = conj(elem);
 
-                    amps[i] += elem * cache[j];
+                    cpu_qcomp term = elem * cache[j];
+                    qreal re = term.re - correction.re;
+                    qreal nextRe = sum.re + re;
+                    correction.re = (nextRe - sum.re) - re;
+                    sum.re = nextRe;
 
-                    /// @todo
-                    /// qureg.cpuAmps[i] is being serially updated by only this thread,
-                    /// so is a candidate for Kahan summation for improved numerical
-                    /// stability. Explore whether this is time-free and worthwhile!
-                    ///
-                    /// BEWARE that Kahan summation may be incompatible with
-                    /// the commutator tricks used in base_qcomp's (ancestor
-                    /// of cpu_qcomp) arithmetic operator overloads. Check
-                    /// base_qcomp.hpp before implementing compensation.
+                    qreal im = term.im - correction.im;
+                    qreal nextIm = sum.im + im;
+                    correction.im = (nextIm - sum.im) - im;
+                    sum.im = nextIm;
                 }
+
+                amps[i] = sum;
             }
         }
     }
